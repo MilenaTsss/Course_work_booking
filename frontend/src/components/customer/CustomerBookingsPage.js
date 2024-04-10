142ac6b629b65aa3cd0ecc13ee7736a6be12ff83
MilenaTsss <mvtsarakhova@edu.hse.ru>
2024-04-11 01:09:06 +0300

    added feedback

--- frontend/src/components/customer/CustomerBookingsPage.js
+++ frontend/src/components/customer/CustomerBookingsPage.js
@@ -13,18 +13,30 @@ import {
     Card,
     CardActions,
     CardContent,
-    Container,
+    Container, DialogTitle, DialogContent, DialogContentText, TextField, DialogActions, Dialog,
 } from '@mui/material';
 import LogoutIcon from "@mui/icons-material/Logout";
 
 import {getUser} from "../user/Requests";
-import {} from "../user/Requests";
-import {deleteBooking, getBookings} from "../admin/Requests";
+import {addFeedback, deleteBooking, getBookings} from "../admin/Requests";
 import AccountCircle from "@mui/icons-material/AccountCircle";
 
 export default function CustomerBookingsPage() {
     const [user, setUser] = useState(JSON.parse(Cookies.get('user') || '{}'));
     const [bookings, setBookings] = useState([]);
+    const [dialogOpen, setDialogOpen] = useState(false);
+    const [currentBooking, setCurrentBooking] = useState({
+        id: '',
+        comment: '',
+        rating: 1,
+        // add all other fields from booking here
+        service: {},
+        serviceProvider: {},
+        customer: {},
+        startTime: '',
+        endTime: '',
+        isActive: '',
+    })
     const [error, setError] = useState(null);
     const [success, setSuccess] = useState(null);
     const navigate = useNavigate();
@@ -63,6 +75,23 @@ export default function CustomerBookingsPage() {
         navigate("/login");
     };
 
+    const addFeedbackForBooking = async () => {
+        try {
+            console.log(currentBooking)
+            const response = await addFeedback(
+                currentBooking.id, currentBooking.comment, currentBooking.rating, setError, setSuccess
+            );
+            if (response) {
+                setSuccess("Отзыв успешно оставлен");
+                setDialogOpen(false);
+            } else {
+                setError("Ошибка при добавлении отзыва");
+            }
+        } catch (error) {
+            setError("Ошибка при добавлении отзыва");
+        }
+    };
+
     const deleteExistingBooking = async (bookingId) => {
         await deleteBooking(bookingId, setError, setSuccess);
     };
@@ -75,8 +104,7 @@ export default function CustomerBookingsPage() {
                     <Typography component="h1" variant="h5" sx={{flexGrow: 1, textAlign: 'center'}}>
                         Бронирования
                     </Typography>
-                    <IconButton color="inherit" component={Link}
-                                to="/profile/"> {/* Кнопка для перехода на страницу с бронированиями */}
+                    <IconButton color="inherit" component={Link} to="/profile/">
                         <AccountCircle/>
                     </IconButton>
                     <IconButton color="inherit" onClick={handleLogout}>
@@ -113,10 +141,56 @@ export default function CustomerBookingsPage() {
                             {!booking.is_active && <Typography variant="body2" color="error">Отменено</Typography>}
                         </CardContent>
                         <CardActions sx={{justifyContent: 'space-between'}}>
-                            <Button size="small" onClick={() => deleteExistingBooking(booking.id)}>Delete</Button>
+                            <Button size="small" onClick={() => deleteExistingBooking(booking.id)}>Отменить</Button>
+                            <Button size="small" onClick={() => {
+                                setCurrentBooking({
+                                    ...currentBooking,
+                                    ...booking, // spread all properties of booking into currentBooking
+                                });
+                                setDialogOpen(true);
+                            }}>Оставить оценку</Button>
                         </CardActions>
                     </Card>
                 )}
+                <Dialog open={dialogOpen} onClose={() => setDialogOpen(false)}>
+                    <DialogTitle>{"Добавить отзыв"}</DialogTitle>
+
+                    <DialogContent>
+                        <DialogContentText>
+                            {"Заполните поля ниже"}
+                        </DialogContentText>
+                        <TextField
+                            autoFocus
+                            margin="dense"
+                            label="Комментарий"
+                            type="text"
+                            fullWidth
+                            variant="standard"
+                            value={currentBooking ? currentBooking.comment : ""}
+                            onChange={e => {
+                                setCurrentBooking({...currentBooking, comment: e.target.value});
+                            }}
+                        />
+                        <TextField
+                            margin="dense"
+                            label="Рейтинг"
+                            type="text"
+                            fullWidth
+                            variant="standard"
+                            value={currentBooking ? currentBooking.rating : ""}
+                            onChange={e => {
+                                setCurrentBooking({...currentBooking, rating: e.target.value});
+                            }}
+                        />
+                    </DialogContent>
+                    <DialogActions>
+                        <Button onClick={() => setDialogOpen(false)}>Cancel</Button>
+                        <Button onClick={() => {
+                            addFeedbackForBooking()
+                            setDialogOpen(false);
+                        }}>Сохранить</Button>
+                    </DialogActions>
+                </Dialog>
 
                 {error && (
                     <Alert severity="error" sx={{mt: 5, width: '100%', textAlign: 'center'}}>{error}</Alert>
