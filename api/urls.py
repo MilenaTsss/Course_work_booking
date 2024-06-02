88e78e64762364a07864050428d95ae9434c16b5
Tsarakhova Milena <milena.czaraxova@gmail.com>
2024-06-02 22:21:02 +0300

    changed paths

--- api/urls.py
+++ api/urls.py
@@ -9,16 +9,16 @@ urlpatterns = [
     path('user/<int:user_id>/', views.UserView.as_view()),
 
     path('services/<int:business_id>/', views.ServicesView.as_view()),
-    path('service/<int:business_id>/<int:service_id>/', views.ServiceView.as_view()),
+    path('services/<int:business_id>/<int:service_id>/', views.ServiceView.as_view()),
 
     path('providers/<int:business_id>/', views.ProvidersView.as_view()),
-    path('provider/<int:business_id>/<int:provider_id>/', views.ProviderView.as_view()),
+    path('providers/<int:business_id>/<int:provider_id>/', views.ProviderView.as_view()),
 
-    path('provider/<int:business_id>/<int:provider_id>/schedule/', views.ScheduleView.as_view()),
-    path('provider/<int:business_id>/<int:provider_id>/schedule-item/', views.ScheduleItemView.as_view()),
+    path('providers/<int:business_id>/<int:provider_id>/schedule/', views.ScheduleView.as_view()),
+    path('providers/<int:business_id>/<int:provider_id>/schedule-item/', views.ScheduleItemView.as_view()),
 
     path('bookings/', views.BookingsView.as_view()),
     path('booking/<int:booking_id>/', views.BookingView.as_view()),
 
-    path('<int:business_id>/<int:service_id>/<int:service_provider_id>', views.AvailabilityView.as_view()),
+    path('businesess/<int:business_id>/<int:service_id>/<int:service_provider_id>', views.AvailabilityView.as_view()),
 ]
